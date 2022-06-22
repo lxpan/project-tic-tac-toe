6aff441fc6f8414340281b9fd3c59eda0b8ccd4d
Luoxi Pan <l.panpax@gmail.com>
2022-06-22 18:31:18 +1000

    Create empty objects for structure

--- game.js
+++ game.js
@@ -1,1 +1,31 @@
 // Project: Tic Tac Toe
+
+// *--------- Modules
+
+// represents current state of the game board (and actions that players can take?)
+const gameBoard = {
+
+}
+
+// render contents of gameBoard array to webpage
+const displayController = {
+
+}
+
+// the outermost object
+const gameRunner = {
+
+}
+
+// *--------- Factories
+
+const playerFactory = {
+
+}
+
+players = []
+player1 = playerFactory('Luoxi', 'human');
+player2 = playerFactory('Hal', 'AI');
+players.push(player1, player2);
+
+const gameInstance = gameRunner(players, gameBoard);
