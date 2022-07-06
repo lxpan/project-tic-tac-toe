9ba936b7403c0e04c3e84e2f898e00e6be39edc1
Luoxi Pan <l.panpax@gmail.com>
2022-07-06 12:31:03 +1000

    Add modal

--- game.js
+++ game.js
@@ -189,6 +189,7 @@ const game = ( (doc) => {
     const _resetGame = () => {
         // board.boardState = board.resetBoard();
         // board.resetBoard();
+        // 
         board.boardState = [['', '', ''], ['', '', ''], ['', '', '']];
 
         victoryStatus = {
@@ -249,3 +250,21 @@ const gameRunner = ( () => {
 
 const runner = gameRunner;
 runner.run();
+
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector(".trigger");
+const closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
